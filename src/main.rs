8b5226e8f69e1d6c5f0ef666cb1e67dac5da06eb
Abdul Haris Djafar <abdulharisdjafar@Abduls-MacBook-Pro.local>
2025-02-14 06:05:04 +0700

    update code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,22 @@ use todolist_rust;
 use future_rust;
 
 fn main() -> Result<(), std::io::Error> {
-    future_rust::run();
+    let data = &[2,3,1];
+    print!("{:?}",p(data));
     Ok(())
 
+}
+
+fn p(p: &[i32]) -> Vec<i32> {
+    let len = p.len() as i32;
+
+    (1..=len)
+        .map(|t| {
+            p.iter().position(|&x| x == t).map(|idx| idx as i32 + 1)
+        })
+        .map(|opt| {
+            opt.and_then(|t| p.iter().position(|&x| x == t).map(|idx| idx as i32 + 1))
+        })
+        .collect::<Option<Vec<i32>>>()
+        .unwrap_or_else(|| vec![]) 
 }
