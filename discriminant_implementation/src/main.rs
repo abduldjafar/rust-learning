0bd10e4392eaf409cc1eeee0220ad6dc46b6ac1d
Abdul Haris Djafar <abdulharisdjafar@Abduls-MacBook-Pro.local>
2025-04-07 13:16:32 +0700

    update code make more readable

--- discriminant_implementation/src/main.rs
+++ discriminant_implementation/src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, help="the coefficient of x (the \"linear\" term)")]
+    b: f64,
+    #[arg(short, long, help="the coefficient of x^2 (the \"quadratic\" term)")]
+    a: f64,
+    #[arg(short, long, help="the constant term ")]
+    c: f64,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let d = (cli.b*cli.b) - (4 as f64 *cli.a*cli.c);
+
+    println!("Discriminant : {}", d);
 }
